39deadd88356a6e31faa368127eea78f77312460
Johan Duque <johand.duque@gmail.com>
2021-09-13 10:51:51 +0200

    first working version using infura to load img to ipfs

--- nft-chat/src/App.js
+++ nft-chat/src/App.js
@@ -7,68 +7,41 @@ const client = create('https://ipfs.infura.io:5001/api/v0')
 
 function App() {
   const [fileUrl, updateFileUrl] = useState(``)
-  async function onChange(e) {
-    const file = e.target.files[0]
-    console.log("file", file);
-    try {
-      const added = await client.add(file)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      updateFileUrl(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
-  }
 
-
-  async function onTxtChange(e) {
-    
-
-
-
-
+  async function onSend(e) {
     let canvas = document.createElement("canvas");
-            canvas.id = "canvasId";
-            canvas.width = 300;
-            canvas.height = 200;
-            let ctx = canvas.getContext('2d');
-            ctx.font = "30px Arial";
-            let text = document.getElementById("the_text").value;
-            console.log("the text", text);
+    canvas.id = "canvasId";
+    canvas.width = 600;
+    canvas.height = 200;
+    let ctx = canvas.getContext('2d');
+    ctx.font = "30px Arial";
+    let text = document.getElementById("the_text").value;
+    console.log("the text", text);
 
-            ctx.fillText(text, 10, 50);
-            let img = document.createElement("img");
-            img.src = canvas.toDataURL("image/png");
-            document.getElementById("show_img_here").append(img);
-            //document.body.appendChild(canvas);
-    
-//console.log("img", img.src);
+    ctx.fillText(text, 30, 80);
+    let img = document.createElement("img");
+    img.src = canvas.toDataURL("image/png");
+    document.getElementById("show_img_here").append(img);
 
-//let fileContent = document.getElementById("show_img_here");
+    let fileContent = img.src;
 
-let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
-let fileContent = img.src;
-
-
-let arr = fileContent.split(','), mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while(n--){
-        u8arr[n] = bstr.charCodeAt(n);
+    let arr = fileContent.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
     }
-    let file = new File([u8arr], 'filename.png', {type:mime});
 
-
+    let file = new File([u8arr], 'filename.png', { type: mime });
 
 
     try {
       const added = await client.add(file)
       const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      
+      console.log("new file url:", url)
       updateFileUrl(url)
     } catch (error) {
       console.log('Error uploading file: ', error)
     }
-    
   }
 
 
@@ -76,24 +49,39 @@ let arr = fileContent.split(','), mime = arr[0].match(/:(.*?);/)[1],
     <div className="App">
       <h1>IPFS Example</h1>
       <input
-        type="file"
-        onChange={onChange}
-      />
-      <input
         id="the_text"
         type="text"
-        
+        style={{
+          /* backgroundColor: "#4CAF50", Green */
+          padding: "13px 50px",
+          textAlign: 'center',
+          textDecoration: 'none',
+          fontSize: "16px",
+
+          width: '300px'
+        }}
+
       />
-      <button
-      width="300"
-      height="300"
-      onClick={onTxtChange}
-      />
+      <button width="300" height="300" style={{
+        backgroundColor: "#4CAF50", /* Green */
+        border: 'none',
+        color: 'white',
+        padding: "15px 32px",
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: "inline-block",
+        fontSize: "16px",
+        margin: "4px 2px",
+        cursor: "pointer",
+        /* width: 35px; */
+      }}
+        onClick={onSend}
+      >Send</button>
+
       <div id="show_img_here"></div>
-      <canvas width="600" height="400"></canvas>
       {
         fileUrl && (
-          <img src={fileUrl} width="600px" eight="400px"/>
+          <img src={fileUrl} width="600px" height="200px" />
         )
       }
     </div>
