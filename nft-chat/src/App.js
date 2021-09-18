5801cfab4e389d2bd439d8cfdf53b255f7a7b60c
Johan Duque <johand.duque@gmail.com>
2021-09-18 17:31:26 +0200

    upload metadata to ipfs

--- nft-chat/src/App.js
+++ nft-chat/src/App.js
@@ -1,13 +1,10 @@
 /* src/App.js */
 import './App.css'
-import { useState } from 'react'
 import { create } from 'ipfs-http-client'
 import Web3 from 'web3'
 import React, { Component } from 'react'
 
 const client = create('https://ipfs.infura.io:5001/api/v0')
-
-
 class App extends Component {
 
   constructor(props) {
@@ -18,7 +15,6 @@ class App extends Component {
       network: ''
     }
   }
-
 
   //lifecycle function componenet
   async componentWillMount() {
@@ -69,76 +65,49 @@ class App extends Component {
   }
 
   async onSend(e) {
-    var [fileUrl, updateFileUrl] = ``
     let canvas = document.createElement("canvas");
     canvas.id = "canvasId";
     canvas.width = 600;
     canvas.height = 200;
     let ctx = canvas.getContext('2d');
-    ctx.font = "30px Arial";
+    ctx.font = "20px Courier New";
+    ctx.fontFamily = "'Courier New', monospace;"
     let text = document.getElementById("the_text").value;
-    console.log("the text", text);
-
     ctx.fillText(text, 30, 80);
+
     let img = document.createElement("img");
     img.src = canvas.toDataURL("image/png");
     document.getElementById("show_img_here").append(img);
 
-    let fileContent = img.src;
+    const imgFile= getImageFile(img.src);
+    const imgUrl = await uploadFileToIPFS(imgFile);
 
-    let arr = fileContent.split(','), mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    let file = new File([u8arr], 'filename.png', { type: mime });
-
-
-    try {
-      const added = await client.add(file)
-      const url = `https://ipfs.infura.io/ipfs/${added.path}`
-      console.log("new file url:", url)
-      updateFileUrl(url)
-    } catch (error) {
-      console.log('Error uploading file: ', error)
-    }
+    //Upload Metadata
+    const metadata = getMetaData(imgUrl);
+    const metaJson = JSON.stringify(metadata);
+    const metadataFile = new File([metaJson], 'metadata.json', { type: 'text/plain;charset=UTF-8' });
+    console.log("FILE METADATA", metadataFile);
+    uploadFileToIPFS(metadataFile);
   }
 
-
   async App() {
-
     this.setState({ loading: false })
   }
 
 
   render() {
-    let content
-    if (this.state.loading) {
-      content = <img src={'https://media.giphy.com/media/l3nWhI38IWDofyDrW/giphy.gif'}
-        width="80"
-        height="80" />
-    }
-    let fileUrl = ''
-
     return (
       <div className="App">
         <h>{this.state.account}<br></br></h>
         <h>{this.state.network}</h>
         <h1>IPFS Example</h1>
-        <input
+
+        <textarea
           id="the_text"
-          type="text"
-          style={{
-            /* backgroundColor: "#4CAF50", Green */
-            padding: "13px 50px",
-            textAlign: 'center',
-            textDecoration: 'none',
-            fontSize: "16px",
-
-            width: '300px'
-          }}
-
+          value={this.state.textAreaValue}
+          onChange={this.handleChange}
+          rows={5}
+          cols={5}
         />
         <button width="300" height="300" style={{
           backgroundColor: "#4CAF50", /* Green */
@@ -157,11 +126,6 @@ class App extends Component {
         >Send</button>
 
         <div id="show_img_here"></div>
-        {
-          fileUrl && (
-            <img src={fileUrl} width="600px" height="200px" />
-          )
-        }
       </div>
     )
 
@@ -171,3 +135,82 @@ class App extends Component {
 }
 
 export default App
+
+function getImageFile(fileContent) {
+  const arr = fileContent.split(',')
+  const mime = arr[0].match(/:(.*?);/)[1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  let u8arr = new Uint8Array(n)
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+
+  const imgFile = new File([u8arr], 'filename.png', { type: mime })
+  return imgFile;
+}
+
+async function uploadFileToIPFS(file) {
+  let fileUrl;
+  try {
+    const added = await client.add(file)
+    fileUrl = `https://ipfs.infura.io/ipfs/${added.path}`
+    console.log("File url:", fileUrl)
+  } catch (error) {
+    console.log('Error uploading file: ', error)
+  }
+  return fileUrl
+}
+
+function getMetaData(imgUrl) {
+  return {
+    "name": "Powered by NFT Chat",
+    "description": "We gotta decide what to write here, the msg it self?, adds?, Our Marketing msg?",
+    "image": `${imgUrl}`,
+    "attributes": [{
+      "trait_type": "Base",
+      "value": "Starfish"
+    },
+    {
+      "trait_type": "Eyes",
+      "value": "Big"
+    },
+    {
+      "trait_type": "Level",
+      "value": 5
+    },
+    {
+      "trait_type": "Stamina",
+      "value": 1.4
+    },
+    {
+      "trait_type": "Personality",
+      "value": "Sad"
+    },
+    {
+      "display_type": "boost_number",
+      "trait_type": "Aqua Power",
+      "value": 40
+    },
+    {
+      "display_type": "boost_percentage",
+      "trait_type": "Stamina Increase",
+      "value": 10
+    },
+    {
+      "display_type": "number",
+      "trait_type": "Generation",
+      "value": 2
+    },
+    {
+      "display_type": "date",
+      "trait_type": "birthday",
+      "value": 8566360800 //Thing about this
+    },
+    {
+      "value": "Simple property"
+    }
+    ]
+  }
+}
