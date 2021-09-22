25f284b76bf687620d6fac1f7a550e62068e87f4
Johan Duque <johand.duque@gmail.com>
2021-09-22 18:04:42 +0200

    description fixed

--- nft-chat/src/NFTMetadata.js
+++ nft-chat/src/NFTMetadata.js
@@ -1,7 +1,9 @@
 export function getMetaData(imgUrl, senderAdress, birthday) {
     return {
         "name": "nftchat.xyz",
-        "description": `${birthday} \n You received this message from ${senderAdress} \n To reply visit nftchat.xyz`,
+        "description": `${birthday} \n
+You received this message from ${senderAdress} \n
+To reply visit nftchat.xyz`,
         "image": `${imgUrl}`,
         "attributes": [{
             "display_type": "date",
