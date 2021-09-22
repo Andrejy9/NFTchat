24c75d5e8bd565a3c959f4458c20ee8f1a025b5f
Johan Duque <johand.duque@gmail.com>
2021-09-22 16:02:27 +0200

    nft metadata

--- nft-chat/src/NFTMetadata.js
+++ nft-chat/src/NFTMetadata.js
@@ -1,51 +1,12 @@
 export function getMetaData(imgUrl, senderAdress, birthday) {
     return {
         "name": "nftchat.xyz",
-        "description": `You received this message from: ${senderAdress}`,
+        "description": `You received this message from ${senderAdress}. To reply visit nftchat.xyz`,
         "image": `${imgUrl}`,
         "attributes": [{
-                "trait_type": "Base",
-                "value": "Starfish"
-            },
-            {
-                "trait_type": "Eyes",
-                "value": "Big"
-            },
-            {
-                "trait_type": "Level",
-                "value": 5
-            },
-            {
-                "trait_type": "Stamina",
-                "value": 1.4
-            },
-            {
-                "trait_type": "Personality",
-                "value": "Sad"
-            },
-            {
-                "display_type": "boost_number",
-                "trait_type": "Aqua Power",
-                "value": 40
-            },
-            {
-                "display_type": "boost_percentage",
-                "trait_type": "Stamina Increase",
-                "value": 10
-            },
-            {
-                "display_type": "number",
-                "trait_type": "Generation",
-                "value": 2
-            },
-            {
-                "display_type": "date",
-                "trait_type": "birthday",
-                "value": `${birthday}`
-            },
-            {
-                "value": "Simple property"
-            }
-        ]
+            "display_type": "date",
+            "trait_type": "Message Date",
+            "value": `${birthday}`
+        }]
     }
 }
